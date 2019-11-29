0b4f8a3916d14b0990fb8b8fce901d257856d95d
Jung-woo Lee <jwl@Jung-wooui-MacBook.local>
2019-11-29 18:53:40 +0900

    d

--- farmsharing_project/farmsharing_project/settings.py
+++ farmsharing_project/farmsharing_project/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2m%=rfi5@x1mh$+40b)noxu-g9_y%m6^)ajll6(6ovdfv@h&g4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+Debug = bool(os.environ.get('DJANGO_DEBUG',True))
+
 
 ALLOWED_HOSTS = []
 AUTH_USER_MODEL ='accounts.Profile'
@@ -54,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'farmsharing_project.urls'
@@ -132,3 +135,7 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+import dj_database_url
+db_from_env=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
